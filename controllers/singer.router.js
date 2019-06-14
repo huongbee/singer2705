67f3f1222949b22b77c0539bbba56f9d0c550f07
Huong <huongnguyenak96@gmail.com>
2019-06-14 20:27:32 +0700

    view

--- controllers/singer.router.js
+++ controllers/singer.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { arraySinger, Singer, avatarLink, profileLink } = require('../models/Singer');
 
 router.get('/',(req,res)=>{
-    
+    res.render('index');
 })
 router.get('/add',(req,res)=>{
     res.send('add')
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const singerRouter = require('./controllers/singer.router');
+app.set('view engine','ejs');
 app.use('/', singerRouter);
 
 app.listen(3000);
